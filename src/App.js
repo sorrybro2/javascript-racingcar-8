820938c924419fad788703cf7b5c5fac5e5d6d00
IM_SORRY <imwe25@naver.com>
2025-10-23 12:19:41 +0900

    feat : 자동자 전진 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const car_list = await MissionUtils.Console.readLineAsync();
+    const count = await MissionUtils.Console.readLineAsync();
+
+    const car_racing = {};
+
+    car_list.forEach((name) => car_racing[name] = 0);
+
+    for(let i = 0; i < count; i++){
+      car_list.forEach((name) => {
+        const random_num = MissionUtils.Random.pickNumberInRange(0, 9);
+        if(random_num >= 4){
+          car_racing[name] += 1;
+        }
+      });
+    }
+
+  }
 }
 
 export default App;
